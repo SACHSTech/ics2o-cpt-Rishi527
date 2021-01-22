d0997a58f3bc541331a1851c28c357ca73ce66f5
Rishi Kalathil <rishi.kalathil23@ycdsbk12.ca>
2021-01-22 13:31:06 +0000

    .

--- main.py
+++ main.py
@@ -1,51 +1,82 @@
-""" 
-A basic pygame template
-"""
- 
-import pygame
- 
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
-pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
+import random
+
+def display_intro():
+    title = "** The Tech Quiz **"
+    print("*" * len(title))
+    print(title)
+    print("*" * len(title))
+
+
+def display_menu():
+    menu_list = ["1. Ransomware", "2. Trojan Horse", "3. Worms", "4. Spyware", "5. GPU", "6. CPU", "7. Hard Drive", "8. Keyboard", "9. Random", "10. Exit"]
+    print(menu_list[0])
+    print(menu_list[1])
+    print(menu_list[2])
+    print(menu_list[3])
+    print(menu_list[4])
+    print(menu_list[5]) 
+    print(menu_list[6])
+    print(menu_list[7])
+    print(menu_list[8])
+    print(menu_list[9])
+
+
+
+
 done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
+display_intro() 
+display_menu()
+print("Make sure you enter answers in all lowercase letters")
+while done == False:
+  question = int(input("Enter a number: "))
   
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
+    
+
+  if question == 1:
+    quiz_one = input("Ransomware payments are often demanded in the form of _____, which is like digital gold. (credit cards, bitcoin, checks, bank account numbers): ")
+    quiz_one.lower()
+    if quiz_one == "bitcoin":
+      print("Correct")
+    else: 
+      print("Wrong")
+
+
+  if question == 2:
+    quiz_one = input("Trojan Horses create backdoor access to your computer info for ________. (anyone on the internet, the user who created the malware, everyone in your email address book, anyone with your email address): ")
+    quiz_one.lower()
+    if quiz_one == "the user who created the malware":
+      print("Correct")
+    else: 
+      print("Wrong")
+
+  if question == 3  :
+    quiz_one = input("Which of the following is not a similarity between a worm and virus? (they both cause system or network slowdowns, they both hide in host programs or change their appearance to evade detection, they both can spread through the Internet, they both use host programs to replicate): ")
+    quiz_one.lower()
+    if quiz_one == "they both use host programs to replicate":
+      print("Correct")
+    else: 
+      print("Wrong")
+
+  if question == 4:
+    quiz_one = input("_____ tracks information on a user's viewing habits while on the internet, and then sends that information to a remote computer without the user's knowledge. (buffer overflow, cross-site scripting, worm, spyware): ")
+    quiz_one.lower()
+    if quiz_one == "spyware":
+      print("Correct")
+    else: 
+      print("Wrong")
+
+  if question == 5:
+    quiz_one = input("Which one of these statistics apply to the GPU. (clock speed, refresh rate, switches, dpi): ")
+    quiz_one.lower()
+    if quiz_one == "clock speed":
+      print("Correct")
+    else: 
+      print("Wrong")
+
+if question == 6:
+    quiz_one = input("Which one of these statistics apply to the CPU. (size, cores, watts, efficiency rating): ")
+    quiz_one.lower()
+    if quiz_one == "cores":
+      print("Correct")
+    else: 
+      print("Wrong")
